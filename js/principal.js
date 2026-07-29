a2434ff2c3ea84a76447aad06a176f4bdd4c07c1
Agostino <agostinoscopetta@hotmail.com>
2026-07-29 14:05:38 -0300

    Suma el modal de error a la pagina y su inicializacion desde JavaScript

---
+++ js/principal.js
@@ -0,0 +1,6 @@
+'use strict';
+// Inicializa los modulos de la pagina una vez construido el DOM.
+function iniciarAplicacion() {
+    iniciarModales();
+}
+window.addEventListener('DOMContentLoaded', iniciarAplicacion);
